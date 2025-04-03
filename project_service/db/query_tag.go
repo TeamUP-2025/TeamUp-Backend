e9993543d7e35d4ac86191833c46c84dfeadc7e8
Nanthawat Duang-ead <nanthawat.boom@gmail.com>
2025-04-03 16:54:45 +0700

    add updateProjectdetailwithTag

--- project_service/db/query_tag.go
+++ project_service/db/query_tag.go
@@ -1,24 +1,25 @@
-//package db
-//
-//import (
-//	"context"
-//	"github.com/jackc/pgx/v5"
-//	"github.com/jackc/pgx/v5/pgtype"
-//)
-//
-//func UpdateProjectDetailWithTag(projectId string, databaseUrl string) ([]Repo, error) {
-//	ctx := context.Background()
-//
-//	conn, err := pgx.Connect(ctx, databaseUrl)
-//	if err != nil {
-//		return getProjectByProjectIdRow{}, err
-//	}
-//	queries := New(conn)
-//
-//	uuid := pgtype.UUID{}
-//	err = uuid.Scan(projectId)
-//	if err != nil {
-//		return getProjectByProjectIdRow{}, err
-//	}
-//	return queries.getProjectByProjectId(ctx, uuid)
-//}
+package db
+
+import (
+	"context"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+func UpdateProjectDetailWithTag(data updateProjectTitleDescriptionParams, databaseUrl string) error {
+	ctx := context.Background()
+
+	conn, err := pgx.Connect(ctx, databaseUrl)
+
+	queries := New(conn)
+
+	uuid := pgtype.UUID{}
+	err = uuid.Scan(data)
+
+	err = queries.updateProjectTitleDescription(ctx, data)
+	if err != nil {
+		return err
+	}
+
+	return err
+}
